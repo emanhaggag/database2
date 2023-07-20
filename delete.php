28c56a0d805ec570bd3ca8f6b79ac83fb6d33560
emanhaggag <104195788+emanhaggag@users.noreply.github.com>
2023-07-20 14:40:36 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,11 +1,10 @@
 <?php
-
-    
-
+require_once('./init.php');
 
 if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'GET'){
     if(isset($_GET['student_id'])){
         delete_with_id('student',$_GET['student_id']);
+        header("Refresh:3;url=index.php"); 
     }
 }
 
